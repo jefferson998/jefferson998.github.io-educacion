a79ccc03b8885e9dcd8452e3dbec1d7d345eaf9e
jeffersonco <pazjefferson11216@gmail.com>
2021-01-20 19:37:47 -0500

    Clases hasta Search

---
+++ REACT-JS/PlatVideo/src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header'
+import '../assets/styles/App.scss'
+import Search from '../components/Search'
+const App = () =>(
+    <div className="App">
+        <Header />
+        <Search />
+    </div>
+
+);
+
+export default App;
